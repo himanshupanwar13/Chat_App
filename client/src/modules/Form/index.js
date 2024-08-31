f2ab51d9152c92932c28580960137f23541cf3ec
himanshupanwar13 <himanshupanwarxiii@gmail.com>
2024-08-31 23:08:07 +0530

    new commit

--- client/src/modules/Form/index.js
+++ client/src/modules/Form/index.js
@@ -17,7 +17,7 @@ const Form = ({
       <div className="text-4xl font-bold">Welcome {isSignInPage && "Back"}</div>
       <div className="text-xl font-light mb-14">{isSignInPage? "Sign in now to Get Explore" : "Sign up now to Get Started" }</div>
 
-      <form onSubmit={() => console.log("Submitted")} className="flex flex-col w-full items-center " >
+      <form onSubmit={() => console.log("Submitted")} className="flex flex-col w-full justify-center items-center " >
         {!isSignInPage && <Input label="Full name" name="name" placeholder="Enter your FullName" className="mb-6" value={data.fullName} onChange={(e)=> setData({ ...data, fullName : e.target.value})} />}
       <Input label="Email address" name="email" placeholder="Enter your email" type="email" className="mb-6" value={data.email} onChange={(e)=> setData({ ...data, email : e.target.value}) } />
       <Input label="Password" name="password" placeholder="Please enter password" type="Password" className="mb-10" value={data.password} onChange={(e)=> setData({ ...data, password: e.target.value})} />
