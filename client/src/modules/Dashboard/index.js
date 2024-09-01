b4f4c29dde5ae4d44f85c087da2528f40bcbf264
himanshupanwar13 <himanshupanwarxiii@gmail.com>
2024-09-02 01:19:33 +0530

    new changes

--- client/src/modules/Dashboard/index.js
+++ client/src/modules/Dashboard/index.js
@@ -108,8 +108,17 @@ const Dashboard = () => {
             </div>
           </div>
         </div>
-        <div className='p-14 w-full'>
-          <Input type='text' placeholder='Type a message...' className='' inputClassName='w-[75%] p-4 border-0 shadow-lg rounded-full bg-black focus:ring-0 focus:borderr-0' />
+        <div className='p-14 w-full flex items-center'>
+          <div className='w-[85%]'>
+            <Input placeholder='Type a message...' className='w-full p-4 border-0 shadow-lg rounded-full bg-black focus:ring-0 focus:border-0' />
+          </div>
+          <div className='ml-4'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="purple" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icon-tabler-send">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M10 14l11 -11" />
+              <path d="M21 3l-6.5 18a.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a.55 .55 0 0 1 0 -1l18 -6.5" />
+            </svg>
+          </div>
         </div>
       </div>
       <div className='w-[25%] h-screen'></div>
